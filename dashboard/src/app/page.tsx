97d470d1ea6b7373c97dd525a9954a88003ef55a
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2025-07-02 11:16:48 +0200

    quick fix

--- dashboard/src/app/page.tsx
+++ dashboard/src/app/page.tsx
@@ -8,9 +8,9 @@ const MapWithPopups = dynamic(() => import('./components/MapWithPopups'), { ssr:
 export default function Home() {
   const [selectedPopupText, setSelectedPopupText] = useState<string>('1');
   return (
-    <main className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 px-2 pt-6">
+    <main className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-200 px-2 pt-6">
       <div className="max-w-6xl mx-auto">
-        <h1 className="text-3xl font-bold mb-8 text-center">Microscopico Dashboard</h1>
+        <h1 className="text-3xl font-bold mb-8 text-center text-blue-500">Microscopico Dashboard</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div className="order-2 md:order-1">
             <div className="h-[500px] w-full">
